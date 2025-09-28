b87cdd411534aebf3c989352b6efe693aa405827
Gisela Gabernet <gisela.gabernet@gmail.com>
2025-09-27 22:28:40 -0400

    sim sequence_aa

--- bin/simulation.py
+++ bin/simulation.py
@@ -405,7 +405,7 @@ class Stereotyping:
                 all_sequences.append({
                     'generation': gen,
                     'sequence_id': child.sequence_id,
-                    'sequence_aa': child.sequence_aa,
+                    'sequence_vdj_aa': child.sequence_aa,
                     'sequence': child.sequence_nt,
                     'dist_to_target': child.dist_to_target
                 })
