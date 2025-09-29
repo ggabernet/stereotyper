fcd32f2af0a91d6783bf6ed53bac5202609f8fc6
Gisela Gabernet <gisela.gabernet@gmail.com>
2025-09-29 12:48:07 -0400

    do not subsample on preprocessing

--- bin/preprocess_repertoire.py
+++ bin/preprocess_repertoire.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 """
 Preprocess repertoire file by subsampling and translating sequences.
-This script reads a repertoire TSV file, drops rows with incomplete CDR1 sequences,
-generates a new nucleotide sequence column without FWR1, translates the sequences to amino acids. The output is saved to a new TSV file.
+This script reads a repertoire TSV file, drops rows with incomplete CDR1 sequences, and FWR4 sequences,
+generates a new nucleotide sequence column without FWR1, translates the sequences to amino acids,
+and subsamples the data to a specified size. The output is saved to a new TSV file.
 
 Usage:
 python preprocess_repertoire.py --input_repertoire <input_file.tsv> --outname <output_file.tsv>
@@ -22,13 +23,10 @@ alakazam = importr('alakazam')
 # parameters
 parser = argparse.ArgumentParser(description="Preprocess repertoire file")
 parser.add_argument("--input_repertoire", type=str, required=True, help="Path to input repertoire TSV file")
-# Do not subsample here as we want to use all sequences for the selection step
-#parser.add_argument("--subsample_size", type=int, default=100000, help="Number of rows to subsample")
 parser.add_argument("--outname", type=str, default=None, help="Output file name (optional)")
 args = parser.parse_args()
 
 input_repertoire = args.input_repertoire
-#subsample_size = args.subsample_size
 if args.outname:
     outname = args.outname
 else:
@@ -78,9 +76,6 @@ translation = list(alakazam.translateDNA(repertoire["sequence_nt_nofwr1"].tolist
 
 
 repertoire["sequence_vdj_aa"] = translation
-
-
-#subset = repertoire.sample(n=subsample_size, random_state=42)
 
 
 repertoire.to_csv(outname, sep="\t", index=False)
